d85272e7ec27dcaf91dcc28b9e523c2cd8b87c55
Lau Kar Rui <karrui.lau@gmail.com>
2019-11-05 16:10:56 +0800

    fix: copywriting of round details

--- containers/RoundSection/index.js
+++ containers/RoundSection/index.js
@@ -1,18 +1,17 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Link from "next/link";
-import Countdown from "react-countdown-now";
-import Box from "~/components/Box";
-import Fade from "react-reveal/Fade";
-import Card from "~/components/Card";
-import Image from "~/components/Image";
-import Text from "~/components/Text";
-import Heading from "~/components/Heading";
-import Button from "~/components/Button";
-import FeatureBlock from "~/components/FeatureBlock";
-import Container from "~/components/UI/Container";
-import { RoundWrapper } from "./roundSection.style";
-import ControlImage from "~/assets/image/crypto/round.svg";
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+import Countdown from 'react-countdown-now';
+import Box from '~/components/Box';
+import Fade from 'react-reveal/Fade';
+import Image from '~/components/Image';
+import Text from '~/components/Text';
+import Heading from '~/components/Heading';
+import Button from '~/components/Button';
+import FeatureBlock from '~/components/FeatureBlock';
+import Container from '~/components/UI/Container';
+import { RoundWrapper } from './roundSection.style';
+import ControlImage from '~/assets/image/crypto/round.svg';
 
 const Completionist = () => (
   <span className="readMore">You are good to go!</span>
@@ -68,13 +67,13 @@ const RoundSection = ({
   btnStyle,
   sectionSubTitle,
   cardArea,
-  btnTitle
+  btnTitle,
 }) => {
   return (
     <RoundWrapper id="control">
       <Container>
         <Box className="row" {...row}>
-          <Box className="colleft" {...col} style={{ flexDirection: "column" }}>
+          <Box className="colleft" {...col} style={{ flexDirection: 'column' }}>
             <Image
               src={ControlImage}
               className="roundImage"
@@ -92,8 +91,8 @@ const RoundSection = ({
                 {...btnTitle}
                 content={
                   roundIsOpen
-                    ? "Round is currently open."
-                    : "Round is currently closed."
+                    ? 'Round is currently open.'
+                    : 'Round is currently closed. The round will open when enough sellers have placed their asks. You may still place your asks or bids while waiting for the round to open.'
                 }
               />
             </Box>
@@ -110,7 +109,7 @@ const RoundSection = ({
               <Link href="https://app.acquity.io">
                 <a>
                   <Button
-                    title="JOIN THE ROUND"
+                    title="GO TO APP"
                     className="countDownMainButton"
                     {...btnStyle}
                   />
@@ -132,7 +131,7 @@ RoundSection.propTypes = {
   row: PropTypes.object,
   col: PropTypes.object,
   btnTitle: PropTypes.object,
-  btnStyle: PropTypes.object
+  btnStyle: PropTypes.object,
 };
 
 // Transactions default style
@@ -140,70 +139,70 @@ RoundSection.defaultProps = {
   // Transactions section row default style
   row: {
     flexBox: true,
-    flexWrap: "wrap",
-    ml: "-15px",
-    mr: "-15px"
+    flexWrap: 'wrap',
+    ml: '-15px',
+    mr: '-15px',
   },
   // Transactions section col default style
   col: {
-    pr: "15px",
-    pl: "15px",
+    pr: '15px',
+    pl: '15px',
     width: [1, 1 / 2, 1 / 2, 1 / 2, 1 / 2],
     flexBox: true,
-    alignSelf: "center"
+    alignSelf: 'center',
   },
 
   // Transactions section title default style
   title: {
-    content: "Take control of your identity in our round-based system",
-    fontSize: ["24px", "26px", "30px", "36px", "40px"],
-    lineHeight: ["30px", "32px", "40px", "50px", "55px"],
-    fontWeight: "700",
-    color: "#32325d",
-    letterSpacing: "-0.010em",
-    mb: "20px",
-    maxWidth: ["100%", "100%", "100%", "100%", "415px"],
-    textAlign: ["left", "left"]
+    content: 'Take control of your identity in our round-based system',
+    fontSize: ['24px', '26px', '30px', '36px', '40px'],
+    lineHeight: ['30px', '32px', '40px', '50px', '55px'],
+    fontWeight: '700',
+    color: '#32325d',
+    letterSpacing: '-0.010em',
+    mb: '20px',
+    maxWidth: ['100%', '100%', '100%', '100%', '415px'],
+    textAlign: ['left', 'left'],
   },
   // Transactions section description default style
   description: {
     content:
-      "Acquity uses weekly rounds to collect anonymised bids and asks, before matching them with our algorithm. Simply join before the round ends and get notified when you match.",
-    fontSize: "16px",
-    fontWeight: "400",
-    color: "#525f7f",
-    lineHeight: "28px",
-    mb: ["30px", "30px", "35px", "35px", "45px"],
-    textAlign: ["left", "left"],
-    maxWidth: ["100%", "100%", "100%", "100%", "430px"]
+      'Acquity uses weekly rounds to collect anonymised bids and asks, before matching them with our algorithm. Simply join before the round ends and get notified when you match.',
+    fontSize: '16px',
+    fontWeight: '400',
+    color: '#525f7f',
+    lineHeight: '28px',
+    mb: ['30px', '30px', '35px', '35px', '45px'],
+    textAlign: ['left', 'left'],
+    maxWidth: ['100%', '100%', '100%', '100%', '430px'],
   },
   sectionSubTitle: {
-    content: "Effortless trading for everyone",
-    as: "span",
-    textAlign: "left",
-    fontSize: ["16px", "16px", "18px", "20px", "20px"],
+    content: 'Effortless trading for everyone',
+    as: 'span',
+    textAlign: 'left',
+    fontSize: ['16px', '16px', '18px', '20px', '20px'],
     fontFamily: "'Nexa Bold','Poppins'",
-    fontWeight: "600",
-    lineHeight: "27px",
-    color: "#525f7f",
-    textAlign: ["left", "left"]
+    fontWeight: '600',
+    lineHeight: '27px',
+    color: '#525f7f',
+    textAlign: ['left', 'left'],
   },
   // Button default style
   btnStyle: {
-    minWidth: "156px",
-    fontSize: "14px",
-    fontWeight: "500"
+    minWidth: '156px',
+    fontSize: '14px',
+    fontWeight: '500',
   },
   btnTitle: {
-    as: "span",
-    fontSize: ["18px", "18px", "20px", "20px", "20px"],
-    lineHeight: ["25px", "27px", "27px", "27px", "27px"],
-    fontWeight: "500",
-    color: "#32325d",
-    letterSpacing: "-0.010em",
-    mb: "10px",
-    textAlign: ["left", "left"]
-  }
+    as: 'span',
+    fontSize: ['18px', '18px', '20px', '20px', '20px'],
+    lineHeight: ['25px', '27px', '27px', '27px', '27px'],
+    fontWeight: '500',
+    color: '#32325d',
+    letterSpacing: '-0.010em',
+    mb: '10px',
+    textAlign: ['left', 'left'],
+  },
 };
 
 export default RoundSection;
